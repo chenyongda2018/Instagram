2af21f1a5c9b081fefde5cc46e305ebc436c7574
chenyongda <chenchenyongda@gmail.com>
2020-08-19 22:49:46 +0800

    add space

--- app/src/main/java/com/example/instagram/homePage/MainActivity.java
+++ app/src/main/java/com/example/instagram/homePage/MainActivity.java
@@ -38,9 +38,9 @@ public class MainActivity extends AppCompatActivity implements OnFeedItemClickLi
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
         initView();
 
         if (savedInstanceState == null) {
